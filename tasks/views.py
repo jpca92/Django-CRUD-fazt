0444cef6fa48d6fb601d64dbe69870c6ea149fde
jpca92 <jpca92@gmail.com>
2024-08-01 09:22:56 -0500

    min 00:36:50 funcionando

--- tasks/views.py
+++ tasks/views.py
@@ -25,8 +25,14 @@ def signup(request):
                 user.save()
                 return HttpResponse('User created successfully')
             except:
-                return HttpResponse('Username already exists') 
-        return HttpResponse('Password do not match')
+                return render(request, 'signup.html',{
+                    'form': UserCreationForm,
+                    'error': 'Username already exists'
+                }) 
+        return render(request, 'signup.html',{
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
 
         print(request.POST)
         print('obteniendo datos' )
