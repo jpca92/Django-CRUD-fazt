1d20c10caad326de13d586f197879bbc52db1a9f
jpca92 <jpca92@gmail.com>
2024-08-01 22:11:15 -0500

    min 01:20:07 funcionando

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+# creamos una clase para poder visualizar la fecha de creacion
+# la fecha de creacion la dejamos auto por eso no se ve inicialmente
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', )
+
+
+# Importamos el modelo de Task
+# ponemos la siguiente linea para que aparezca en el panel de admin
+admin.site.register(Task, TaskAdmin)
+
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title=models.CharField(max_length=100)
+    description= models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important= models.BooleanField(default=False)
+
+    # Django ya tiene la entidad User creada por el formulario de las vistas
+    # Solo debemos importar el modulo user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__ (self):
+        return self.title + ' - '  + self.user.username
